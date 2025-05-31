6ef21f30ddd2bf98bb5bd5d55ed9d63c381ea8ed
Eliana Bracciaforte <bracciaforte+github@gmail.com>
2025-05-31 12:43:43 -0300

    test: adjust submit form test mocks

--- __mocks__/radix-mock.js
+++ __mocks__/radix-mock.js
@@ -1,9 +1,23 @@
+const React = require("react")
+
+/**
+ * Creates a very simple mock component that renders a basic DOM element
+ * preserving props and children. Radix UI components are heavily styled and
+ * rely on complex behaviours which we don't need during tests. Rendering them
+ * as plain <div> elements keeps the DOM structure predictable while avoiding
+ * implementation details.
+ */
+const createMockComponent = (element = "div") =>
+  React.forwardRef(({ children, ...props }, ref) =>
+    React.createElement(element, { ref, ...props }, children),
+  )
+
 module.exports = new Proxy(
   {},
   {
-    get: () =>
-      new Proxy(() => {}, {
-        get: () => () => ({}),
-      }),
+    get: (target, prop) => {
+      if (prop === "__esModule") return true
+      return createMockComponent()
+    },
   },
 )
